75bd46cd4a05812686866e94baa8df0b6fbd7605
rocas <rocasdev@proton.me>
2025-12-15 00:42:13 +0200

    chore: bump version to 2.0.0

--- components/VersionTag.tsx
+++ components/VersionTag.tsx
@@ -3,7 +3,7 @@ import { Badge } from '@/components/ui/badge'
 export default function VersionTag() {
   return (
     <Badge variant={'secondary'} className='fixed bottom-3 left-3 z-9999 opacity-70'>
-      v0.1.1
+      v2.0.0
     </Badge>
   )
 }
